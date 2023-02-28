b885dbe126a1d85f3413116e52431276fcf9ae85
BGreider <bgreider@gmail.com>
2023-02-28 23:36:28 +0000

    GreiderLab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,7 +20,10 @@ public class Lab {
      *
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
      */
-    public void mustThrow() {
-
+int i = 0;
+    public void mustThrow() throws Exception{
+        if(i==0){
+            throw new Exception("Zero!");
+        }
     }
 }
